a62c26b7d6f307bb628c0b809bb79c67f2652a19
Ivan Novakov <ivan.novakov@debug.cz>
2013-04-18 10:48:32 +0200

    added examples

---
+++ examples/client.php
@@ -0,0 +1,46 @@
+<?php
+use InoPerunApi\Client\Http;
+use InoPerunApi\Client\Serializer\Json;
+use InoPerunApi\Client\Client;
+use InoPerunApi\Client\Request;
+use InoPerunApi\Client\Authenticator\BasicAuth;
+
+include __DIR__ . '/_common.php';
+
+$config = require __DIR__ . '/_client_config.php';
+ 
+/*
+ * HTTP client
+ */
+$httpClientFactory = new Http\ClientFactory();
+$httpClient = $httpClientFactory->createClient($config['http_client']);
+
+/*
+ * Serializer
+ */
+$serializer = new Json();
+
+/*
+ * Authenticator
+ */
+$authenticator = new BasicAuth($config['authenticator']);
+
+/*
+ * Perun client
+ */
+$client = new Client($config['client'], $httpClient, $serializer);
+$client->setAuthenticator($authenticator);
+
+/*
+ * Perun request
+ */
+$perunRequest = new Request('usersManager', 'getUserById', array(
+    'id' => 13793
+));
+
+$perunResponse = $client->send($perunRequest);
+_dump($client->getHttpClient()
+    ->getLastRawRequest());
+_dump($client->getHttpClient()
+    ->getLastRawResponse());
+_dump($perunResponse);
