b98902fea660ce22195dde64255bfd04a85282d1
Ivan Novakov <ivan.novakov@debug.cz>
2013-04-23 21:06:39 +0200

    manager skeleton

---
+++ src/InoPerunApi/Manager/GenericManager.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace InoPerunApi\Manager;
+
+use InoPerunApi\Client\Client;
+class GenericManager
+{
+    
+    
+    public function __construct($managerName, Client $client, $entityFactory = null)
+    {
+        
+    }
+}
